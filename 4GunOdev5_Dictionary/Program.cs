bf4d387df8e8247c9d00955730e2a56c2451d276
Galip <Galip@DESKTOP-HRK9HK1>
2021-03-16 18:55:45 +0300

    Add project files.

---
+++ 4GunOdev5_Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4GunOdev5_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<string, string> urunler = new Dictionary<string, string>();
+            urunler.Add("Kazak","Yünlü,Kaşmir,Pamuklu"); //adding a key/value using the Add() method
+            urunler.Add("Pantolon","Gabardin,Kot,Günlük");
+            urunler.Add("Gömlek", "Pamuklu,Yünlü,Keten,Yazlık");
+
+            foreach (var kvp in urunler)
+                Console.WriteLine("Anahtar: {0}, Değer: {1}", kvp.Key, kvp.Value);
+        }
+    }
+}
